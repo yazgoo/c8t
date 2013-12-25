ef8137c4440ab3407e74fc2e489dda0c23e16583
Olivier Abdesselam <yazgoo@gmail.com>
2013-12-26 00:54:48 +0100

    adding hex input

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,20 @@
 require "opal"
 require "assembler"
 require "emulator"
-assembly = Assembler.new.parse(`text = document.getElementById("editor").value`).output
-e = Emulator.new(assembly, "Window");
-e.run_multiple
+#assembly = Assembler.new.parse(`text = document.getElementById("editor").value`).output
+class Runner
+    def run text
+        assembly = Assembler.new.parse(text).output
+        p assembly
+        e = Emulator.new(assembly, "Window");
+        e.run_multiple
+        e
+    end
+    def run2 text
+        assembly = text.split("\n").join.scan(/../).map{ |s| s.to_i(16) }
+        p assembly
+        e = Emulator.new(assembly, "Window");
+        e.run_multiple
+        e
+    end
+end
